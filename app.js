59a0b6e00c19d0e7ba4d5cf31ed61ad54a7c5caa
Thanuja D <76948739+ThanujaDayananda@users.noreply.github.com>
2023-05-21 19:53:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('views'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-mongoose.connect('mongodb://localhost:27017/project',{
+mongoose.connect('mongodb+srv://thanujadayananda05:Thanuja2405@cluster0.zxajxng.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedtopology:true
 });
